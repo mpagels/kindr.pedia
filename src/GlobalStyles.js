8b34816470ca86f991bd2c25d93ca5d97a3eeabe
Martin <office@martinpagels.de>
2021-08-04 22:16:26 +0200

    Install styled components

---
+++ src/GlobalStyles.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+
+* {
+    box-sizing: border-box;
+}
+
+body {
+    margin: 0;
+    background-color:  #F6F8FC;
+    color: #000000;
+    font-family: 'Montserrat', sans-serif;
+}
+
+:root {
+ 
+}`;
